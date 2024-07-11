1dffcb8fb39b683cd7d0e2509fb051e66f7b2e05
Arda Ordu <ardaordu03@gmail.com>
2024-07-11 13:46:54 +0100

    Add comment explaining the calculateCost function.

--- src/paintBrand.ts
+++ src/paintBrand.ts
@@ -44,3 +44,48 @@ export class PaintBrand {
     }
 }
 
+// 1 litre: £2.00
+// 2.5 litres: £4.00
+// 5 litres: £8.00
+// 10 litres: £16.00
+
+// Example Calculation for 25 Litres:
+// Initial Values:
+
+// totalCost = 0
+// totalCans = 0
+// remainingVolume = 25
+
+// Sorted Can Sizes:
+
+// [10 litres, 5 litres, 2.5 litres, 1 litre]
+
+// Using 10-Litre Cans:
+
+// First 10-litre can:
+
+// remainingVolume = 25 - 10 = 15
+// totalCost = 0 + 16 = 16
+// totalCans = 0 + 1 = 1
+// Second 10-litre can:
+
+// remainingVolume = 15 - 10 = 5
+// totalCost = 16 + 16 = 32
+// totalCans = 1 + 1 = 2
+
+// Using 5-Litre Can:
+
+// One 5-litre can:
+// remainingVolume = 5 - 5 = 0
+// totalCost = 32 + 8 = 40
+// totalCans = 2 + 1 = 3
+
+// Remaining Volume Check:
+
+// remainingVolume = 0, so no need for an additional can.
+
+// Final Result:
+
+// totalCans = 3
+// totalCost = 40
+// usedCanSizes = [{ volume: 10, price: 16.00 }, { volume: 10, price: 16.00 }, { volume: 5, price: 8.00 }]
